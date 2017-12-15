7343a1b67859ddf44aa3d2a62dd780924b9f1033
Sam Li <samli@google.com>
2017-12-14 17:37:44 -0800

    Rewrite client to use Typescript and new API format (#40)
    The API typings are refactored in a top level `api.d.ts` file which is used by both the client and the server.

---
+++ client/dash.ts
@@ -0,0 +1,67 @@
+import {DashResponse, PullRequest} from '../api'
+import {html, render} from './node_modules/lit-html/lit-html.js'
+
+async function start() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const endpoint = queryParams.has('test') ? '/test-dash.json' : '/dash.json';
+  const res = await fetch(endpoint, {credentials: 'include'});
+  const json = await res.json() as DashResponse;
+
+  const pullRequestTemplate = (pr: PullRequest) => html`
+    <div class="pr">
+      <img class="avatar" src="${pr.avatarUrl}">
+      <div class="item-contents">
+        <div class="header">
+          ${pr.repository} <b>${pr.title}</b> #${pr.number}
+        </div>
+        <div class="footer">
+          <div>
+            Approved by nobody
+          </div>
+          <div>
+            Line 2
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const tmpl = html`
+    <style>
+      body {
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 14pt;
+      }
+      .pr {
+        padding: 12px;
+        height: 68px;
+        display: flex;
+      }
+      .avatar {
+        width: 52px;
+        height: 52px;
+        border: 4px solid #26A745;
+        border-radius: 50%;
+        margin-right: 16px;
+      }
+      .item-contents {
+        display: flex;
+        flex-direction: column;
+      }
+
+      .header {
+        font-size: 18px;
+        margin-bottom: 4px;
+      }
+
+      .footer {
+        font-size: 16px;
+        line-height: 22px;
+      }
+    </style>
+    ${json.prs.map(pullRequestTemplate)}
+  `;
+  render(tmpl, document.body);
+}
+
+start();
