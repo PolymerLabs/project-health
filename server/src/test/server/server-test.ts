8109a1a44655ef6c63641218a007f696fdbc348d
Sam Li <samli@google.com>
2017-12-15 16:03:08 -0800

    Add tests for dashboard server (#39)
    This change uses dependency injection to pass the GitHub client into the dashboard server to allow for testing. The test then re-uses the recording/replay server for testing against the GitHub API. The test uses a test account `project-health1` so that tests are real, but unchanging. To re-record the API responses, any GitHub token must be used with the test user still fixed.

--- server/src/test/server/server-test.ts
+++ server/src/test/server/server-test.ts
@@ -1,10 +1,52 @@
-import test from 'ava';
-import {mergeObjects} from '../../server';
+import * as ava from 'ava';
 
-test('mergeObjects should merge objects', (t) => {
-  t.deepEqual({a: 1}, mergeObjects({a: 1}, {a: 2}));
-  t.deepEqual({a: {b: [1, 2]}}, mergeObjects({a: {b: [1]}}, {a: {b: [2]}}));
-  t.deepEqual(
-      {a: {b: [1, 2], c: 3}},
-      mergeObjects({a: {b: [1], c: 3}}, {a: {b: [2], c: 4}}));
+import {DashServer} from '../../server';
+import {startTestReplayServer} from '../replay-server';
+
+/**
+ * Assigns the test context object before each test to ensure it is correctly
+ * typed.
+ */
+function contextualize<T>(getContext: (_: ava.TestContext) => Promise<T>):
+    ava.RegisterContextual<T> {
+  ava.test.beforeEach(async (t) => {
+    Object.assign(t.context, await getContext(t));
+  });
+  return ava.test;
+}
+
+/**
+ * Generates the test context object before each test.
+ */
+const test = contextualize(async (t) => {
+  const {server, client} = await startTestReplayServer(t);
+  return {
+    replayServer: server,
+    client,
+    dash: new DashServer(client),
+    // This token must be set in the environment during recording.
+    token: process.env.GITHUB_TOKEN || '',
+  };
+});
+
+test.afterEach.cb((t) => {
+  t.context.replayServer.close(t.end);
+});
+
+test('basic PR', async (t) => {
+  const result =
+      await t.context.dash.fetchUserData('project-health1', t.context.token);
+  t.deepEqual(result, {
+    prs: [{
+      repository: 'project-health1/repo',
+      title: 'Update README.md',
+      number: 1,
+      avatarUrl: 'https://avatars3.githubusercontent.com/u/34584679?v=4',
+      approvedBy: [],
+      changesRequestedBy: [],
+      commentedBy: [],
+      pendingReviews: [],
+      statusState: 'passed'
+    }]
+  });
 });
