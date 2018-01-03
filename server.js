5d7c46d436483def9f37171d23bc85019807b67b
Matt Gaunt <matt@gauntface.co.uk>
2018-01-02 17:44:59 -0800

    Adding secrets.json as a quick way to support secrets with appengine

--- server.js
+++ server.js
@@ -3,5 +3,14 @@
 const {DashServer} = require('./server/lib/server');
 const {GitHub} = require('./server/lib/github');
 
+try {
+  const secrets = require('./secrets.json');
+  if (secrets['GITHUB_CLIENT_SECRET']) {
+    process.env.GITHUB_CLIENT_SECRET = secrets['GITHUB_CLIENT_SECRET'];
+  }
+} catch (err) {
+  // No secrets defined.
+}
+
 const server = new DashServer(new GitHub());
 server.listen();
