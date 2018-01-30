0171b4c13d8ae8b52608d41f05adc408a22bd6ae
Sam Li <samli@google.com>
2018-01-30 15:26:38 -0800

    Allow dashboard param to view other logins using your own token

--- src/client/dash.ts
+++ src/client/dash.ts
@@ -112,9 +112,9 @@ function prTemplate(pr: api.PullRequest) {
 
 async function start() {
   const queryParams = new URLSearchParams(window.location.search);
-  const loginParams = queryParams.get('login') ? `?login=${queryParams.get('login')}` : '';
-  const res = await fetch(
-      `/dash.json${loginParams}`, {credentials: 'include'});
+  const loginParams =
+      queryParams.get('login') ? `?login=${queryParams.get('login')}` : '';
+  const res = await fetch(`/dash.json${loginParams}`, {credentials: 'include'});
   const json = await res.json() as api.DashResponse;
 
   const tmpl = html`
