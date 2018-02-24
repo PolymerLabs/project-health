6e22a211ad29cd0674ac77e15e25d21222962af4
Sam Li <samli@google.com>
2018-02-23 18:10:10 -0800

    increase threshold

--- src/pixel-tester.ts
+++ src/pixel-tester.ts
@@ -38,7 +38,7 @@ function imagesMatch(
       const diff = new PNG({width: img1.width, height: img1.height});
       const matches =
           pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {
-            threshold: 0
+            threshold: 0.1
           }) === 0;
 
       // Write out diff file.
