4a423d22a388fb16736c6eb792cab724f78f4ec8
Sam Li <samli@google.com>
2018-02-26 15:39:31 -0800

    plz

---
+++ src/custom_typings/merge-img.d.ts
@@ -0,0 +1,16 @@
+declare module 'merge-img' {
+import {Jimp} from 'jimp';
+
+  export default function mergeImg(
+      images: Array<string|
+                    {src: string | Buffer, offsetX?: number, offsetY?: number}|
+                    Buffer|Jimp>,
+      options?: {
+        direction?: boolean,
+        color?: number,
+        align?: 'start'|'center'|'end',
+        offset?: number,
+        margin?: number|string|
+              {top?: number, right?: number, bottom?: number, left?: number},
+      }): Promise<Jimp>;
+}
