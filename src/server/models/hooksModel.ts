b8d1ebcdd1eb9c9cc165e921f0a57e8accda1ee3
Matt Gaunt <matt@gauntface.co.uk>
2018-03-08 17:29:55 -0800

    Use actual firestore transactions

--- src/server/models/hooksModel.ts
+++ src/server/models/hooksModel.ts
@@ -10,14 +10,15 @@ class HooksModel {
    * in a the doc already existing.
    */
   async logHook(hookDelivery: string): Promise<boolean> {
-    const hookDoc =
+    const hookDocRef =
         await firestore().collection(HOOK_COLLECTION_NAME).doc(hookDelivery);
-    return firestore().runTransaction(async () => {
-      const docSnapshot = await hookDoc.get();
-      if (docSnapshot.exists) {
+    return firestore().runTransaction(async (transaction) => {
+      const hookDoc = await transaction.get(hookDocRef);
+      if (hookDoc.exists) {
         return false;
       }
-      await hookDoc.create({received: true, timestamp: Date.now()});
+      await transaction.create(
+          hookDocRef, {received: true, timestamp: Date.now()});
       return true;
     });
   }
