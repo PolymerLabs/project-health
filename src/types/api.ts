4c99f305793471dff978456b50bd4b121c4d6ba9
Matt Gaunt <matt@gauntface.co.uk>
2018-03-16 16:43:32 -0700

    Adding reload on new version

--- src/types/api.ts
+++ src/types/api.ts
@@ -77,9 +77,14 @@ export interface JSONAPIResponse<T> {
   data?: T;
 }
 
-export interface LoginResponse { status: string; }
+export interface LoginResponse {
+  status: string;
+}
 
-export interface LastKnownResponse { lastKnownUpdate: string|null; }
+export interface LastKnownResponse {
+  lastKnownUpdate: string|null;
+  version: string|null;
+}
 
 /** Not necessarily actionable. */
 
@@ -187,7 +192,9 @@ export interface ErrorPayload {
   message: string;
 }
 
-export interface NotificationURLData { url: string; }
+export interface NotificationURLData {
+  url: string;
+}
 
 export type SWClientMessage<T> = {
   action: 'push-received',
