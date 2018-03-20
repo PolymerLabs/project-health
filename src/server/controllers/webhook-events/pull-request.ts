6aa0178b229ba796f9eca97f9e515aa453dd496b
Matt Gaunt <matt@gauntface.co.uk>
2018-03-20 12:24:35 -0700

    Clean firstore after the PR has closed

--- src/server/controllers/webhook-events/pull-request.ts
+++ src/server/controllers/webhook-events/pull-request.ts
@@ -1,3 +1,4 @@
+import {PullRequest} from '../../../types/api';
 import {WebHookHandleResponse} from '../../apis/github-webhook';
 import {getPRTag, sendNotification} from '../../controllers/notifications';
 import {pullRequestsModel} from '../../models/pullRequestsModel';
@@ -42,12 +43,22 @@ async function handlePROpened(hookBody: PullRequestHook):
   return {handled: true, notifications: null, message: null};
 }
 
+async function handlePRClosed(hookBody: PullRequestHook) {
+  const owner = hookBody.repository.owner.login;
+  const repo = hookBody.repository.name;
+  const num = hookBody.pull_request.number;
+  await pullRequestsModel.deletePR(owner, repo, num);
+  return {handled: true, notifications: null, message: null};
+}
+
 export async function handlePullRequest(hookBody: PullRequestHook):
     Promise<WebHookHandleResponse> {
   if (hookBody.action === 'review_requested') {
     return handleReviewRequested(hookBody);
   } else if (hookBody.action === 'opened') {
     return handlePROpened(hookBody);
+  } else if (hookBody.action === 'closed') {
+    return handlePRClosed(hookBody);
   }
 
   return {
