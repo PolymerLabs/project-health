4626d20e6e0469ac689299ea76c61d985147bdec
Sam Li <samli@google.com>
2018-03-21 16:06:39 -0700

    Fix CI auto-deployment by updating to use .gcloudignore

--- src/test/project-setup-test.ts
+++ src/test/project-setup-test.ts
@@ -1,41 +1,37 @@
 import {test} from 'ava';
 import * as crypto from 'crypto';
-import * as fs from 'fs-extra';
-import * as yamlParser from 'js-yaml';
+import * as fsExtra from 'fs-extra';
 import * as path from 'path';
+
 
 test('should skip all top level directories', async (t) => {
   const WHITELIST = ['build'];
 
   const projectRoot = path.join(__dirname, '..', '..');
-  const folderContents = await fs.readdir(projectRoot);
+  const folderContents = await fsExtra.readdir(projectRoot);
   const directoryNames = [];
   for (const folderItemPath of folderContents) {
-    const stats = await fs.stat(path.join(projectRoot, folderItemPath));
+    const stats = await fsExtra.stat(path.join(projectRoot, folderItemPath));
     if (stats.isDirectory()) {
       directoryNames.push(folderItemPath);
     }
   }
 
-  const yamlFileContents =
-      await fs.readFile(path.join(projectRoot, 'app.yaml'));
-  const appYaml = yamlParser.safeLoad(yamlFileContents.toString());
-
   const dirMissingFromSkipList = [];
-  const skipFiles = appYaml.skip_files;
+  const gcloudIgnore =
+      await fsExtra.readFile(path.join(projectRoot, '.gcloudignore'));
+  const skipLines = gcloudIgnore.toString().split('\n');
   for (const dirName of directoryNames) {
-    if (skipFiles.indexOf(`${dirName}/`) === -1 &&
+    if (skipLines.indexOf(`${dirName}/`) === -1 &&
         WHITELIST.indexOf(dirName) === -1) {
       dirMissingFromSkipList.push(dirName);
     }
   }
 
   if (dirMissingFromSkipList.length > 0) {
-    console.error(
-        'There are directories in the project root which aren\'t ' +
-        'skipped or whitelisted.');
-    console.error(dirMissingFromSkipList);
-    t.fail();
+    t.fail(
+        `These directories in the project root aren't skipped or whitelisted: ${
+            dirMissingFromSkipList}`);
   } else {
     t.pass();
   }
