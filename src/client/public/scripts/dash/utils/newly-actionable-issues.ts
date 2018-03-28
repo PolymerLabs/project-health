fc0205ec49aafeb3cb6fa79623aa1dcb58dba431
Matt Gaunt <matt@gauntface.co.uk>
2018-03-27 17:15:16 -0700

    Altering which issues are newly actionable
    Fixes #364

--- src/client/public/scripts/dash/utils/newly-actionable-issues.ts
+++ src/client/public/scripts/dash/utils/newly-actionable-issues.ts
@@ -4,12 +4,11 @@ export function newlyActionableIssues(
     newList: Issue[], oldList: Issue[]): string[] {
   const result: string[] = [];
 
+  // All issues are actionable
   const oldIds = oldList.map((issue: Issue) => issue.id);
 
   for (const newIssue of newList) {
-    // All issues are actionable
-
-    if (oldIds.indexOf(newIssue.id) !== -1) {
+    if (oldIds.indexOf(newIssue.id) === -1) {
       // New list has an actionable issue that didn't exist before
       result.push(newIssue.id);
     }
