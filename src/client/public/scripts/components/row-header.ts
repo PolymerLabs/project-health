3512049a370778faf005add64333f4e5f2c89dff
Sam Li <samli@google.com>
2018-04-12 14:37:01 -0700

    Factor out a row-header element (#427)

---
+++ src/client/public/scripts/components/row-header.ts
@@ -0,0 +1,102 @@
+import {html} from '../../../../../node_modules/lit-html/lib/lit-extended.js';
+import {TemplateResult} from '../../../../../node_modules/lit-html/lit-html.js';
+import {timeToString} from '../dash/utils/time-to-string.js';
+
+import {BaseElement} from './base-element.js';
+import {DashboardRowData} from './dashboard-row.js';
+
+export class RowHeader extends BaseElement {
+  private _rowData: DashboardRowData|undefined = undefined;
+  private _extraHeaderData: TemplateResult[]|undefined = undefined;
+
+  constructor() {
+    super();
+  }
+
+  get rowData(): DashboardRowData|undefined {
+    return this._rowData;
+  }
+
+  set rowData(data: DashboardRowData|undefined) {
+    this._rowData = data;
+    this.requestRender();
+  }
+
+  get extraHeaderData() {
+    return this._extraHeaderData;
+  }
+
+  set extraHeaderData(data: TemplateResult[]|undefined) {
+    this._extraHeaderData = data;
+    this.requestRender();
+  }
+
+  render() {
+    async function handleRowClick(event: Event) {
+      if (!data) {
+        return;
+      }
+
+      let anchorElement: HTMLElement|null = event.target as HTMLElement;
+      while (anchorElement &&
+             !anchorElement.classList.contains('dashboard-row-link')) {
+        anchorElement = anchorElement.parentNode as HTMLElement;
+      }
+
+      if (anchorElement) {
+        anchorElement.removeAttribute('has-new-activity');
+      }
+
+      const response = await fetch('/api/last-viewed/update/', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({
+          id: data.id,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        const responseText = await response.text();
+        console.warn('Unable to update last-viewed timestamp: ', responseText);
+      }
+    }
+
+    const data = this._rowData;
+    if (!data) {
+      return html``;
+    }
+
+    return html`
+<div class="dashboard-row-author">
+  <div class="dashboard-row-author__name">${data.author}</div>
+    <time class="dashboard-row-author__creation-time" datetime="${
+        new Date(data.createdAt).toISOString()}">${
+        timeToString(data.createdAt)}</time>
+</div>
+
+<div class="dashboard-row-avatar">
+  <img class="dashboard-row-avatar__img" src="${data.avatarUrl}">
+</div>
+
+<a class="dashboard-row-link" href="${data.url}" target="_blank" on-click="${
+        handleRowClick}" has-new-activity$="${data.hasNewActivity}">
+  <div class="dashboard-row-status small-heading">
+    <span class="dashboard-row-status__msg">${data.status.text}</span>
+    <span class="dashboard-row-status__has-activity"></span>
+  </div>
+  <div class="dashboard-row-info">
+    <span class="dashboard-row-info__repo-name">${data.owner}/${
+        data.repo}</span>
+    <span class="dashboard-row-info__title">${data.title}</span>
+  </div>
+</a>
+
+${this.extraHeaderData}
+`;
+  }
+}
+
+customElements.define('row-header', RowHeader);
