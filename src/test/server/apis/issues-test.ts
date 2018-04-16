06f9f473deb0f2a0e33702e46d1c55ae59373ba0
Matt Gaunt <matt@gauntface.co.uk>
2018-04-16 16:11:09 -0700

    Moving issues API to private API router (#441)

--- src/test/server/apis/issues-test.ts
+++ src/test/server/apis/issues-test.ts
@@ -5,7 +5,6 @@ import {SinonSandbox} from 'sinon';
 
 import {handleActivityIssues, handleAssignedIssues} from '../../../server/apis/issues';
 import {userModel} from '../../../server/models/userModel';
-import {IssuesResponse} from '../../../types/api';
 import {initFirestore} from '../../../utils/firestore';
 import {github, initGithub} from '../../../utils/github';
 import {newFakeUserRecord} from '../../utils/newFakeUserRecord';
@@ -17,18 +16,6 @@ type TestContext = {
   sandbox: SinonSandbox,
 };
 const test = anyTest as TestInterface<TestContext>;
-
-// tslint:disable-next-line: no-any
-function newFakeResponse(): any {
-  const response = {
-    status: () => {
-      return response;
-    },
-    send: () => {},
-    json: () => {}
-  };
-  return response;
-}
 
 function newFakeRequest() {
   return {
@@ -63,79 +50,52 @@ test.afterEach.always(async (t) => {
   });
 });
 
-test.serial('[assignedIssues]: should return 401 when no record', async (t) => {
-  t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-      .callsFake(() => {
-        return null;
-      });
-
-  const response = newFakeResponse();
-  const statusSpy = t.context.sandbox.spy(response, 'status');
-  const sendSpy = t.context.sandbox.spy(response, 'send');
-  const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-  await handleAssignedIssues(newFakeRequest(), response);
-
-  t.deepEqual(statusSpy.args[0][0], 401, 'Status code');
-  t.deepEqual(sendSpy.callCount, 1, 'send() call count');
-  t.deepEqual(jsonSpy.callCount, 0, 'json() call count');
-});
-
 test.serial(
     '[assignedIssues]: should retrieve issues for a user (Using replays)',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => {
-            const user = newFakeUserRecord();
-            user.username = 'project-health1';
-            return user;
-          });
+      const userRecord = newFakeUserRecord();
+      userRecord.username = 'project-health1';
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-      await handleAssignedIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, [
-        {
-          id: 'MDU6SXNzdWUzMDgyMTA0Njc=',
-          title: 'I found a bug.',
-          repo: 'repo',
-          owner: 'project-health1',
-          author: 'project-health2',
-          avatarUrl: 'https://avatars3.githubusercontent.com/u/34584974?v=4',
-          createdAt: 1521849856000,
-          url: 'https://github.com/project-health1/repo/issues/16',
-          popularity: 1,
-          hasNewActivity: false,
-          status: {type: 'Assigned'},
-        },
-        {
-          id: 'MDU6SXNzdWUzMDgyMTAyNzM=',
-          title: 'Self Assigned Issue',
-          repo: 'repo',
-          owner: 'project-health1',
-          author: 'project-health1',
-          avatarUrl: 'https://avatars3.githubusercontent.com/u/34584679?v=4',
-          createdAt: 1521849762000,
-          url: 'https://github.com/project-health1/repo/issues/15',
-          popularity: 1,
-          hasNewActivity: false,
-          status: {type: 'Assigned'},
-        },
-      ]);
+      const response = await handleAssignedIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data, {
+        issues: [
+          {
+            id: 'MDU6SXNzdWUzMDgyMTA0Njc=',
+            title: 'I found a bug.',
+            repo: 'repo',
+            owner: 'project-health1',
+            author: 'project-health2',
+            avatarUrl: 'https://avatars3.githubusercontent.com/u/34584974?v=4',
+            createdAt: 1521849856000,
+            url: 'https://github.com/project-health1/repo/issues/16',
+            popularity: 1,
+            hasNewActivity: false,
+            status: {type: 'Assigned'},
+          },
+          {
+            id: 'MDU6SXNzdWUzMDgyMTAyNzM=',
+            title: 'Self Assigned Issue',
+            repo: 'repo',
+            owner: 'project-health1',
+            author: 'project-health1',
+            avatarUrl: 'https://avatars3.githubusercontent.com/u/34584679?v=4',
+            createdAt: 1521849762000,
+            url: 'https://github.com/project-health1/repo/issues/15',
+            popularity: 1,
+            hasNewActivity: false,
+            status: {type: 'Assigned'},
+          },
+        ]
+      });
     });
 
 test.serial(
     '[assignedIssues]: should retrieve issues for a user with a login query parameter',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => newFakeUserRecord());
-
       const githubInstance = github();
       t.context.sandbox.stub(githubInstance, 'query').callsFake((options) => {
         t.deepEqual(
@@ -145,28 +105,22 @@ test.serial(
         return {data: {search: {}}};
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
+      const userRecord = newFakeUserRecord();
       const request = newFakeRequest();
       request.query = {
         login: 'query-username',
       };
 
-      await handleAssignedIssues(request, response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, []);
+      const response = await handleAssignedIssues(request, userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, []);
     });
 
 test.serial(
     '[assignedIssues]: Should filter out empty issue nodes', async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => newFakeUserRecord());
-
       const githubInstance = github();
       t.context.sandbox.stub(githubInstance, 'query').callsFake(() => {
         return {
@@ -180,23 +134,17 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-      await handleAssignedIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, []);
+      const userRecord = newFakeUserRecord();
+      const response = await handleAssignedIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, []);
     });
 
 test.serial(
     '[assignedIssues]: Should filter out non-issue nodes', async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => newFakeUserRecord());
-
       const githubInstance = github();
       t.context.sandbox.stub(githubInstance, 'query').callsFake(() => {
         return {
@@ -210,24 +158,18 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-      await handleAssignedIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, []);
+      const userRecord = newFakeUserRecord();
+      const response = await handleAssignedIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, []);
     });
 
 test.serial(
     '[assignedIssues]: Should filter out issue nodes without author',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => newFakeUserRecord());
-
       const githubInstance = github();
       t.context.sandbox.stub(githubInstance, 'query').callsFake(() => {
         return {
@@ -241,27 +183,18 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-      await handleAssignedIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, []);
+      const userRecord = newFakeUserRecord();
+      const response = await handleAssignedIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, []);
     });
 
 test.serial(
     '[assignedIssues]: should retrieve issues for a user with new activity',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => {
-            const record = newFakeUserRecord();
-            record.featureLastViewed.enabledAt = 0;
-            return record;
-          });
       t.context.sandbox.stub(userModel, 'getAllLastViewedInfo')
           .callsFake(() => {
             return {
@@ -340,16 +273,15 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
+      const userRecord = newFakeUserRecord();
+      userRecord.featureLastViewed.enabledAt = 0;
 
-      await handleAssignedIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, [
+      const response = await handleAssignedIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, [
         {
           id: '123',
           title: 'test-title',
@@ -382,12 +314,6 @@ test.serial(
 test.serial(
     '[assignedIssues]: should handle issues for another user and *not* use last viewed',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => {
-            const record = newFakeUserRecord();
-            record.featureLastViewed.enabledAt = 0;
-            return record;
-          });
       t.context.sandbox.stub(userModel, 'getAllLastViewedInfo').callsFake(() => {
         t.fail(
             'getAllLastViewedInfo should not be called when looking at a different user');
@@ -463,19 +389,18 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
+      const userRecord = newFakeUserRecord();
+      userRecord.featureLastViewed.enabledAt = 0;
 
       const request = newFakeRequest();
       request.query = {login: 'different-user'};
 
-      await handleAssignedIssues(request, response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, [
+      const response = await handleAssignedIssues(request, userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, [
         {
           id: '123',
           title: 'test-title',
@@ -508,12 +433,6 @@ test.serial(
 test.serial(
     '[assignedIssues]: should handle when an issue has no last activity (i.e. author of issue is the current user)',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => {
-            const record = newFakeUserRecord();
-            record.username = 'test-user';
-            return record;
-          });
       t.context.sandbox.stub(userModel, 'getAllLastViewedInfo')
           .callsFake(() => {
             return {
@@ -560,16 +479,15 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
+      const userRecord = newFakeUserRecord();
+      userRecord.username = 'test-user';
+      const response = await handleAssignedIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
 
-      await handleAssignedIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, [{
+      t.deepEqual(response.data.issues, [{
                     id: '123',
                     title: 'test-title',
                     repo: 'test-repo',
@@ -584,51 +502,22 @@ test.serial(
                   }]);
     });
 
-test.serial('[issueActivity]: should return 401 when no record', async (t) => {
-  t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-      .callsFake(() => {
-        return null;
-      });
-
-  const response = newFakeResponse();
-  const statusSpy = t.context.sandbox.spy(response, 'status');
-  const sendSpy = t.context.sandbox.spy(response, 'send');
-  const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-  await handleActivityIssues(newFakeRequest(), response);
-
-  t.deepEqual(statusSpy.args[0][0], 401, 'Status code');
-  t.deepEqual(sendSpy.callCount, 1, 'send() call count');
-  t.deepEqual(jsonSpy.callCount, 0, 'json() call count');
-});
-
 test.serial(
     '[issueActivity]: should retrieve issues for a user (Using replays)',
     async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => {
-            const user = newFakeUserRecord();
-            user.username = 'project-health1';
-            return user;
-          });
+      const userRecord = newFakeUserRecord();
+      userRecord.username = 'project-health1';
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-      await handleActivityIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, []);
+      const response = await handleActivityIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, []);
     });
 
 test.serial(
     '[issueActivity]: should return Involved status types', async (t) => {
-      t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-          .callsFake(() => newFakeUserRecord());
-
       const githubInstance = github();
       t.context.sandbox.stub(githubInstance, 'query').callsFake(() => {
         return {
@@ -668,16 +557,13 @@ test.serial(
         };
       });
 
-      const response = newFakeResponse();
-      const sendSpy = t.context.sandbox.spy(response, 'send');
-      const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-      await handleActivityIssues(newFakeRequest(), response);
-
-      t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-      t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-      const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-      t.deepEqual(responsePayload.issues, [{
+      const userRecord = newFakeUserRecord();
+      const response = await handleActivityIssues(newFakeRequest(), userRecord);
+      t.is(response.statusCode, 200, 'Response status code');
+      if ('error' in response) {
+        throw new Error('Expected a data response');
+      }
+      t.deepEqual(response.data.issues, [{
                     id: '123',
                     title: 'test-title',
                     repo: 'test-repo',
@@ -693,13 +579,6 @@ test.serial(
     });
 
 test.serial('[issueActivity]: should return Author status types', async (t) => {
-  t.context.sandbox.stub(userModel, 'getUserRecordFromRequest')
-      .callsFake(() => {
-        const user = newFakeUserRecord();
-        user.username = 'example-user';
-        return user;
-      });
-
   const githubInstance = github();
   t.context.sandbox.stub(githubInstance, 'query').callsFake(() => {
     return {
@@ -739,16 +618,14 @@ test.serial('[issueActivity]: should return Author status types', async (t) => {
     };
   });
 
-  const response = newFakeResponse();
-  const sendSpy = t.context.sandbox.spy(response, 'send');
-  const jsonSpy = t.context.sandbox.spy(response, 'json');
-
-  await handleActivityIssues(newFakeRequest(), response);
-
-  t.deepEqual(sendSpy.callCount, 0, 'send() call count');
-  t.deepEqual(jsonSpy.callCount, 1, 'json() call count');
-  const responsePayload = jsonSpy.args[0][0] as IssuesResponse;
-  t.deepEqual(responsePayload.issues, [{
+  const userRecord = newFakeUserRecord();
+  userRecord.username = 'example-user';
+  const response = await handleActivityIssues(newFakeRequest(), userRecord);
+  t.is(response.statusCode, 200, 'Response status code');
+  if ('error' in response) {
+    throw new Error('Expected a data response');
+  }
+  t.deepEqual(response.data.issues, [{
                 id: '123',
                 title: 'test-title',
                 repo: 'test-repo',
