PolymerLabs/project-health

commit 143e3189b21f1648c723f32ede6840b8096ccc4c

Moving some API's over to a cleaner standard response (#396)

src/server/apis/auto-merge.ts
@@ -1,60 +1,48 @@
-import * as bodyParser from 'body-parser';
 import * as express from 'express';
 
 import {pullRequestsModel} from '../models/pullRequestsModel';
-import {userModel} from '../models/userModel';
+
+import {APIResponse} from './api-router/abstract-api-router';
+import {PrivateAPIRouter} from './api-router/private-api-router';
+import * as responseHelper from './api-router/response-helper';
+
+export async function handleSetMergeOpt(request: express.Request):
+    Promise<APIResponse> {
+  const owner = request.body.owner;
+  const repo = request.body.repo;
+  const num = request.body.number;
+  const automergeOption = request.body.automergeOption;
+
+  if (!automergeOption) {
+    return responseHelper.error(
+        'no-automerge-option', 'You must provide an auto-merge option.');
+  }
+
+  if (!owner) {
+    return responseHelper.error('no-owner', 'You must provide an owner.');
+  }
+
+  if (!repo) {
+    return responseHelper.error('no-repo', 'You must provide a repo.');
+  }
+
+  if (typeof num !== 'number') {
+    return responseHelper.error(
+        'no-pr-number', 'You must provide a valid PR number.');
+  }
+
+  await pullRequestsModel.setAutomergeOptions(
+      owner, repo, num, automergeOption);
+
+  return responseHelper.data({
+    status: 'ok',
+  });
+}
 
 export function getRouter(): express.Router {
-  const automergeRouter = express.Router();
-  automergeRouter.post(
-      '/set-merge-option/',
-      bodyParser.json(),
-      async (request: express.Request, response: express.Response) => {
-        try {
-          if (!request.body) {
-            response.status(400).send('No body.');
-            return;
-          }
-
-          const userRecord = await userModel.getUserRecordFromRequest(request);
-          if (!userRecord) {
-            response.status(401).send('No login details.');
-            return;
-          }
-
-          const owner = request.body.owner;
-          const repo = request.body.repo;
-          const num = request.body.number;
-          const automergeOption = request.body.automergeOption;
-          if (!automergeOption) {
-            response.status(400).send('No automergeOption.');
-            return;
-          }
-
-          if (!owner) {
-            response.status(400).send('No owner.');
-            return;
-          }
-
-          if (!repo) {
-            response.status(400).send('No repo.');
-            return;
-          }
-
-          if (typeof num !== 'number') {
-            response.status(400).send('Invalid number.');
-            return;
-          }
-
-          await pullRequestsModel.setAutomergeOptions(
-              owner, repo, num, automergeOption);
-
-          response.send();
-        } catch (err) {
-          console.error(err);
-          response.status(500).send('An unhandled error occured.');
-        }
-      });
-
-  return automergeRouter;
+  const automergeRouter = new PrivateAPIRouter();
+  automergeRouter.post('/set-merge-options/', handleSetMergeOpt, {
+    requireBody: true,
+  });
+  return automergeRouter.router;
 }

src/client/public/scripts/dash/utils/get-data.ts
@@ -31,8 +31,7 @@ export async function getLastKnownUpdate(): Promise<api.LastKnownResponse> {
         credentials: 'include',
       });
 
-  const fullResponse =
-      await response.json() as api.JSONAPIResponse<api.LastKnownResponse>;
+  const fullResponse = await response.json();
   if (fullResponse.error) {
     throw new Error(
         `Unable to get last known update: ${fullResponse.error.message}`);

src/client/public/scripts/oauth/oauth.ts
@@ -1,5 +1,3 @@
-import {GenericStatusResponse, JSONAPIResponse} from '../../../../types/api.js';
-
 function handleOriginRedirect(
     redirectOrigin: string, queryParams: URLSearchParams) {
   // This redirect allows testing on localhost or on the remote server
@@ -23,8 +21,7 @@ async function handleLoginCode(code: string, queryParams: URLSearchParams) {
     credentials: 'include',
   });
 
-  const jsonResponse =
-      (await response.json()) as JSONAPIResponse<GenericStatusResponse>;
+  const jsonResponse = await response.json();
   if (jsonResponse.error) {
     console.error(jsonResponse.error.message);
     const msgElement = document.querySelector('.oauth-msg');

src/server/apis/api-route.ts
@@ -1,60 +1,0 @@
-import * as express from 'express';
-import {JSONAPIResponse} from '../../types/api';
-
-export interface ResponseDetails<T> extends JSONAPIResponse<T> {
-  statusCode: number;
-  cookies?: {
-    [key: string]: {
-      value: string,
-      options: {},
-    }
-  };
-}
-
-type APIRouteCb<T> = (request: express.Request) => Promise<ResponseDetails<T>>;
-
-/**
- * @param routeCb A callback function that will return an instance of
- * ResponseDetails which is used to response to the origin express request.
- */
-export function createAPIRoute<T>(routeCb: APIRouteCb<T>) {
-  return async (request: express.Request, response: express.Response) => {
-    try {
-      const apiResponse = await routeCb(request);
-      if (response.headersSent) {
-        // Some other middleware responded to the request
-        return;
-      }
-
-      // Set any cookies if they are defined.
-      if (apiResponse.cookies) {
-        Object.keys(apiResponse.cookies).forEach((key) => {
-          if (!apiResponse.cookies) {
-            return;
-          }
-
-          const cookieDetails = apiResponse.cookies[key];
-          response.cookie(key, cookieDetails.value, cookieDetails.options);
-        });
-      }
-
-      // Ensure error OR data are added to the response, not both.
-      const jsonResponse: JSONAPIResponse<T> = {};
-      if (apiResponse.error) {
-        jsonResponse.error = apiResponse.error;
-      } else {
-        jsonResponse.data = apiResponse.data;
-      }
-
-      response.status(apiResponse.statusCode).json(jsonResponse);
-    } catch (err) {
-      response.status(500).json({
-        error: {
-          code: 'uncaught-exception',
-          message: `An uncaught exception was thrown while responding to '${
-              request.url}': '${err.message}'`,
-        },
-      });
-    }
-  };
-}

src/server/apis/api-router/abstract-api-router.ts
@@ -1,0 +1,110 @@
+import * as express from 'express';
+
+import {UserRecord} from '../../models/userModel';
+
+import * as responseHelper from './response-helper';
+
+export interface CookiesObject {
+  [key: string]: {
+    value: string,
+    options: {},
+  };
+}
+
+export interface ErrorAPIResponse {
+  statusCode: number;
+  error: {code: string; message: string;};
+}
+
+export interface DataAPIResponse {
+  statusCode: number;
+  cookies?: CookiesObject;
+  // tslint:disable-next-line:no-any
+  data: any;
+}
+
+export type APIResponse = ErrorAPIResponse|DataAPIResponse;
+
+export type PublicAPICallback = (request: express.Request) =>
+    Promise<APIResponse>;
+
+export type PrivateAPICallback =
+    (request: express.Request, userRecord: UserRecord) => Promise<APIResponse>;
+
+export type APIRouteCallback = PublicAPICallback|PrivateAPICallback;
+
+interface APIRouteOpts {
+  requireBody?: boolean;
+}
+
+export abstract class AbstractAPIRouter<T extends APIRouteCallback> {
+  protected expRouter: express.Router;
+
+  constructor() {
+    this.expRouter = express.Router();
+  }
+
+  get(apiPath: string, callback: T) {
+    this.expRouter.get(apiPath, this.wrapCallback(callback, {}));
+  }
+
+  post(apiPath: string, callback: T, opts: APIRouteOpts = {}) {
+    this.expRouter.post(apiPath, this.wrapCallback(callback, opts));
+  }
+
+  private wrapCallback(callback: T, opts: APIRouteOpts) {
+    return async (request: express.Request, response: express.Response) => {
+      try {
+        // Some other middleware may have responded to the request.
+        if (response.headersSent) {
+          return;
+        }
+
+        // If the api requires a body, we can quickly response.
+        if (opts.requireBody && !request.body) {
+          responseHelper.error(
+              'no-body', 'You must provide a request body for this API.');
+          return;
+        }
+
+        const apiResponse =
+            await this.executeCallback(callback, request, response);
+
+        if ('error' in apiResponse) {
+          response.status(apiResponse.statusCode).json(apiResponse.error);
+        } else {
+          // Set any cookies if they are defined.
+          if (apiResponse.cookies) {
+            Object.keys(apiResponse.cookies).forEach((key) => {
+              if (!apiResponse.cookies) {
+                return;
+              }
+
+              const cookieDetails = apiResponse.cookies[key];
+              response.cookie(key, cookieDetails.value, cookieDetails.options);
+            });
+          }
+
+          response.status(apiResponse.statusCode).json(apiResponse.data);
+        }
+
+      } catch (err) {
+        response.status(500).json({
+          error: {
+            code: 'uncaught-exception',
+            message: 'An uncaught exception was thrown: ' + err.message,
+          },
+        });
+      }
+    };
+  }
+
+  get router() {
+    return this.expRouter;
+  }
+
+  protected abstract executeCallback(
+      callback: T,
+      request: express.Request,
+      response: express.Response): Promise<APIResponse>;
+}

src/server/apis/api-router/private-api-router.ts
@@ -1,0 +1,21 @@
+import * as express from 'express';
+
+import {userModel} from '../../models/userModel';
+
+import {AbstractAPIRouter, PrivateAPICallback} from './abstract-api-router';
+import * as responseHelper from './response-helper';
+
+export class PrivateAPIRouter extends AbstractAPIRouter<PrivateAPICallback> {
+  protected async executeCallback(
+      callback: PrivateAPICallback,
+      request: express.Request) {
+    const userRecord = await userModel.getUserRecordFromRequest(request);
+    if (!userRecord) {
+      return responseHelper.error(
+          'not-signed-in', 'You must be signed in to use this API.', {
+            statusCode: 401,
+          });
+    }
+    return callback(request, userRecord);
+  }
+}

src/server/apis/api-router/public-api-router.ts
@@ -1,0 +1,11 @@
+import * as express from 'express';
+
+import {AbstractAPIRouter, PublicAPICallback} from './abstract-api-router';
+
+export class PublicAPIRouter extends AbstractAPIRouter<PublicAPICallback> {
+  protected async executeCallback(
+      callback: PublicAPICallback,
+      request: express.Request) {
+    return callback(request);
+  }
+}

src/server/apis/api-router/response-helper.ts
@@ -1,0 +1,43 @@
+import {CookiesObject, DataAPIResponse, ErrorAPIResponse} from './abstract-api-router';
+
+interface ResponseOptions {
+  statusCode?: number;
+  cookies?: CookiesObject;
+}
+
+export function error(
+    code: string, message: string, opts?: ResponseOptions): ErrorAPIResponse {
+  opts = opts || {};
+
+  const finalOpts: {statusCode: number} = Object.assign(
+      {
+        statusCode: 400,
+      },
+      opts);
+
+  return {
+    statusCode: finalOpts.statusCode,
+    error: {
+      code,
+      message,
+    },
+  };
+}
+
+export function data<T>(data: T, opts?: ResponseOptions): DataAPIResponse {
+  opts = opts || {};
+
+  const finalOpts: {statusCode: number, cookies?: CookiesObject} =
+      Object.assign(
+          {
+            statusCode: 200,
+            cookies: undefined,
+          },
+          opts);
+
+  return {
+    statusCode: finalOpts.statusCode,
+    cookies: finalOpts.cookies,
+    data,
+  };
+}

commit e5352d46f4f446cd75cc3451f55e95f6953faa2b

Add outgoing page (#440)
This change:
 * adds an outgoing page, which is linked if there are more outgoing pull requests than shown (closes #242)
 * dynamically loads the modules for the page requested
 * fixes auto merge UI
 * fixes auto merge API end point, regressed in #396 
 * removes settings page

gulp-tasks/utils/module-to-bundle.js
@@ -36,6 +36,7 @@ const processScript = (scriptPath, relativePath, destDir) => {
              // Minify the bundled JS
              uglifyPlugin({}, esMinify),
            ],
+           experimentalDynamicImport: true,
          })
       .pipe(source(relativePath))
       // Convert streaming vinyl files to use buffers.

replays/[pixel-test]-project-health1-all-outgoing-UI/OutgoingPullRequests-16215aab2bf613b1495bda1ef64a0293615fa817
@@ -81,7 +81,7 @@
       "cost": 1,
       "limit": 5000,
       "remaining": 4998,
-      "resetAt": "2018-04-12T23:18:41Z",
+      "resetAt": "2018-04-16T22:52:13Z",
       "nodeCount": 150,
       "__typename": "RateLimit"
     }

replays/[pixel-test]-project-health1-all-outgoing-UI/OutgoingPullRequests-be58726dcbec2c1ac5f47c51da959cda535ef093
@@ -706,7 +706,7 @@
       "cost": 1,
       "limit": 5000,
       "remaining": 4999,
-      "resetAt": "2018-04-12T23:18:40Z",
+      "resetAt": "2018-04-16T22:52:13Z",
       "nodeCount": 150,
       "__typename": "RateLimit"
     }

src/client/private-pages/index.html
@@ -17,8 +17,8 @@
 <body>
   <nav-element></nav-element>
   <app-element>
-    <dash-page id="dash" class="main-content"></dash-page>
-    <settings-page id="settings"></settings-page>
+    <dash-page id="dash"></dash-page>
+    <dash-pagination id="all"></dash-pagination>
   </app-element>
 </body>
 

src/client/private-pages/settings.html
@@ -1,34 +1,0 @@
-<!doctype html>
-<html>
-
-<head>
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>Settings</title>
-  <link href="/styles/base.css" rel="stylesheet" />
-  <link href="/styles/settings.css" rel="stylesheet" />
-  <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16">
-</head>
-
-<body>
-  <div class="settings-container">
-    <h1>Settings</h1>
-
-    <section>
-      <h2>Organization Permissions</h2>
-      <p>Each organization must add a webhook before any push notifications can be received by anyone in the organization.
-        <a target="_blank" href="https://github.com/settings/connections/applications/23b7d82aec29a3a1a2a8">Review Project-Health Permissions</a>
-      </p>
-      <div class="org-webhook-list"></div>
-    </section>
-  </div>
-</body>
-
-<script nomodule src="/bundled/scripts/settings/org-webhook-list.js" crossorigin></script>
-<script type="module" src="/scripts/settings/org-webhook-list.js" crossorigin></script>
-
-<script>window.ga = window.ga || function () { (ga.q = ga.q || []).push(arguments) }; ga.l = +new Date;</script>
-<script nomodule src="/bundled/scripts/window-analytics.js" crossorigin></script>
-<script type="module" src="/scripts/window-analytics.js"></script>
-
-</html>

src/client/public/scripts/app/app.ts
@@ -1,5 +1,5 @@
 import './router.js';
-import '../dash/dash.js';
+import '../components/nav-element.js';
 
 /**
  * Manages pages within the app.
@@ -51,11 +51,28 @@ class AppElement extends HTMLElement {
   /**
    * Lazily loads the requested module.
    */
-  loadModule(_module: string) {
-    // TODO(samli): This is disabled till these pages are implemented.
-    // const moduleSpecifier = `/scripts/pages/${module}.js`;
-    // import(moduleSpecifier);
+  async loadModule(module: string) {
+    const moduleSpecifier = `/scripts/pages/${module}.js`;
+    if (supportsDynamicImport) {
+      // eval() prevents browsers which don't support dynamic import from
+      // breaking.
+      eval(`import('${moduleSpecifier}')`);
+    } else {
+      const script = document.createElement('script');
+      script.src = `/bundled/scripts/pages/${module}.js`;
+      document.head.appendChild(script);
+    }
   }
 }
+
+const supportsDynamicImport = (() => {
+  try {
+    // tslint:disable-next-line:no-unused-expression
+    new Function('import("")');
+    return true;
+  } catch (err) {
+    return false;
+  }
+})();
 
 customElements.define('app-element', AppElement);

src/client/public/scripts/app/router.ts
@@ -22,7 +22,8 @@ function globalClickHandler(event: MouseEvent) {
     return;
   }
 
-  window.history.pushState({}, '', path);
+  // Preserve query params
+  window.history.pushState({}, '', path + window.location.search);
   document.body.dispatchEvent(new CustomEvent('url-changed'));
 }
 

src/client/public/scripts/components/nav-element.ts
@@ -19,17 +19,11 @@ export class NavElement extends BaseElement {
       avatarUrl = '/images/incognito.svg';
     }
 
-    const buttons = html`
-<a href="/settings" title="Settings" class="settings">
-  <i class="material-icons-extended">settings</i>
-</a>`;
-
     return html`
 <a class="nav-item selected" href="/">
   <img class="nav-item__avatar" src="${avatarUrl}"
        alt="Avatar of ${this.user.login}" />
   <div class="nav-item__name">${this.user.login}</div>
-  <div class="nav-item-actions">${buttons}</div>
 </a>`;
   }
 

src/client/public/scripts/components/row-element.ts
@@ -39,7 +39,7 @@ export class RowElement extends BaseElement {
   _renderEvent(event: RowEvent) {
     function timeTemplate(time?: number) {
       if (!time) {
-        return '';
+        return html`<time class="dashboard-row-event__time"></time>`;
       }
       return html`<time class="dashboard-row-event__time" datetime="${
           new Date(time).toISOString()}">${timeToString(time)}</time>`;

src/client/public/scripts/dash/dash-data.ts
@@ -64,6 +64,14 @@ class DashData {
     return this.lastPolledOutgoing.user;
   }
 
+  getOutgoingInfo(): api.OutgoingPullRequestInfo|null {
+    if (!this.lastPolledOutgoing) {
+      return null;
+    }
+
+    return this.lastPolledOutgoing;
+  }
+
   getOutgoingPrs(): api.OutgoingPullRequest[] {
     if (!this.lastPolledOutgoing) {
       return [];

src/client/public/scripts/dash/dash-poll-controller.ts
@@ -47,7 +47,7 @@ export class DashPollController {
 
   triggerPoll(id: string) {
     if (!this.pollConfigs[id]) {
-      throw new Error(`Unkown poll id: '${id}'`);
+      throw new Error(`Unknown poll id: '${id}'`);
     }
 
     this.clearTimeout(id);

src/client/public/scripts/dash/outgoing.ts
@@ -1,41 +1,0 @@
-import {asyncAppend} from '../../../../../node_modules/lit-html/lib/async-append.js';
-import {html, render} from '../../../../../node_modules/lit-html/lib/lit-extended.js';
-import * as api from '../../../../types/api.js';
-
-import {outgoingPrTemplate} from './prs.js';
-
-/**
- * Async generator which yields lit-html TemplateResults. This fetches pages
- * from the API and yields each rendered pull request result.
- */
-async function* getNextElement(userLogin: string|null) {
-  const loginParam = userLogin ? `login=${userLogin}` : '';
-  let data: api.OutgoingDashResponse|null = null;
-
-  while (!data || data.hasMore) {
-    // Set new cursor information.
-    const cursorParam: string = data ? `cursor=${data.cursor}` : '';
-    const response = await fetch(
-        `/api/dash/outgoing?${[loginParam, cursorParam].join('&')}`,
-        {credentials: 'include'});
-    data = await response.json() as api.OutgoingDashResponse;
-
-    // Render each PR and yield each result so they can be added to the DOM.
-    for (const pr of data.prs) {
-      yield outgoingPrTemplate(pr);
-    }
-  }
-}
-
-function start() {
-  // This allows you to see another users dashboard.
-  const queryParams = new URLSearchParams(window.location.search);
-  const userLogin = queryParams.get('login');
-  // asyncAppend() expects an async iterable which loops over the iterator and
-  // appends these results to the containing element.
-  render(
-      html`${asyncAppend(getNextElement(userLogin))}`,
-      (document.querySelector('#outgoing') as Element));
-}
-
-start();

src/client/public/scripts/dash/prs.ts
@@ -41,16 +41,24 @@ function applyFilter<T extends api.PullRequest>(
   });
 }
 
+function viewAllTemplate(info: api.OutgoingPullRequestInfo) {
+  return html`<a href="/all" class="view-all__button">View all ${
+      info.totalCount} outgoing pull requests</a>`;
+}
+
 export function outgoingPrListTemplate(
     prList: api.OutgoingPullRequest[],
     filter: FilterState|undefined,
     emptyMessageTitle: string,
-    emptyMessageDescription: string) {
+    emptyMessageDescription: string,
+    info: api.OutgoingPullRequestInfo|null) {
   prList = applyFilter(filter, prList);
   if (prList.length) {
     return html`${prList.map((pr) => {
       return outgoingPrTemplate(pr);
-    })}`;
+    })}
+    ${info && info.hasMore ? viewAllTemplate(info) : ''}
+    `;
   } else {
     return createEmptyMessage(emptyMessageTitle, emptyMessageDescription);
   }

src/client/public/scripts/pages/all.ts
@@ -1,0 +1,53 @@
+import {asyncAppend} from '../../../../../node_modules/lit-html/lib/async-append.js';
+import {html} from '../../../../../node_modules/lit-html/lib/lit-extended.js';
+import {TemplateResult} from '../../../../../node_modules/lit-html/lit-html.js';
+import * as api from '../../../../types/api.js';
+import {BaseElement} from '../components/base-element.js';
+
+import {outgoingPrTemplate} from '../dash/prs.js';
+
+class DashPagination extends BaseElement {
+  constructor() {
+    super();
+    // Required for auto merge UI to work.
+    // TODO: this is really slow as a new fetch is done when interacting with
+    // auto merge UI.
+    document.body.addEventListener(
+        'render-outgoing-request', this.requestRender.bind(this));
+  }
+
+  render(): TemplateResult {
+    const queryParams = new URLSearchParams(window.location.search);
+    const userLogin = queryParams.get('login');
+    return html`
+<h2>Outgoing pull requests</h2>
+<div class="pr-list">
+  ${asyncAppend(this._getNextElement(userLogin))}
+</div>`;
+  }
+
+  /**
+   * Async generator which yields lit-html TemplateResults. This fetches pages
+   * from the API and yields each rendered pull request result.
+   */
+  async * _getNextElement(userLogin: string|null) {
+    const loginParam = userLogin ? `login=${userLogin}` : '';
+    let data: api.OutgoingDashResponse|null = null;
+
+    while (!data || data.hasMore) {
+      // Set new cursor information.
+      const cursorParam: string = data ? `cursor=${data.cursor}` : '';
+      const response = await fetch(
+          `/api/dash/outgoing?${[loginParam, cursorParam].join('&')}`,
+          {credentials: 'include'});
+      data = await response.json() as api.OutgoingDashResponse;
+
+      // Render each PR and yield each result so they can be added to the DOM.
+      for (const pr of data.prs) {
+        yield outgoingPrTemplate(pr);
+      }
+    }
+  }
+}
+
+customElements.define('dash-pagination', DashPagination);

src/client/public/scripts/pages/dash.ts
@@ -1,4 +1,3 @@
-import '../components/nav-element.js';
 import '../components/toggle-element.js';
 import '../components/filter-legend.js';
 import '../dash/push-controller.js';
@@ -189,7 +188,8 @@ class DashPage extends BaseElement {
             dashData.getOutgoingPrs(),
             filterController.getFilter('outgoing-prs'),
             'No outgoing pull requests',
-            'When you open new pull requests, they\'ll appear here.')}
+            'When you open new pull requests, they\'ll appear here.',
+            dashData.getOutgoingInfo())}
   </div>
 </div>
 <div id="incoming-prs">

src/client/public/styles/base.css
@@ -27,6 +27,10 @@ app-element {
   margin: 0 auto;
 }
 
+app-element > :not([selected]) {
+  display: none;
+}
+
 @media (max-width: 1550px) {
   app-element {
     margin-left: calc(var(--nav-size) + var(--grid-size));

src/client/public/styles/dash.css
@@ -20,6 +20,17 @@
   color: var(--secondary-text-color);
 }
 
+.view-all__button {
+  display: block;
+  border: 1px solid var(--light-grey);
+  border-radius: 3px;
+  margin: var(--padding) auto;
+  padding: var(--padding);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .title-container {
   display: flex;
   flex-direction: row;

src/client/public/styles/settings.css
@@ -1,26 +1,0 @@
-@import "./variables/colors.css";
-@import "./variables/dimens.css";
-@import "../../../../node_modules/@material/switch/dist/mdc.switch.min.css";
-
-.settings-toggle-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-bottom: var(--padding);
-}
-
-.settings-toggle-item__toggle {
-  margin-right: calc(var(--padding) * 2);
-}
-
-.settings-toggle-item__details h6, .settings-toggle-item__details p {
-  margin: 0;
-}
-
-.settings-toggle-item__details {
-  font-size: 14px;
-}
-
-.settings-toggle-item__details p {
-  color: var(--secondary-text-color);
-}

src/server/apis/auto-merge.ts
@@ -42,7 +42,7 @@ export async function handleSetMergeOpt(request: express.Request):
 
 export function getRouter(): express.Router {
   const automergeRouter = new PrivateAPIRouter();
-  automergeRouter.post('/set-merge-options/', handleSetMergeOpt, {
+  automergeRouter.post('/set-merge-option/', handleSetMergeOpt, {
     requireBody: true,
   });
   return automergeRouter.router;

src/server/dash-server.ts
@@ -125,6 +125,16 @@ export class DashServer {
     app.use('/api/check-pr-state/', getCheckPRStateRouter());
     app.use('/api/issues/', getIssuesRouter());
     app.use('/api/last-viewed/', getLastViewedRouter());
+
+    // Serve app shell on all other routes.
+    app.get(
+        '/*',
+        requireLogin(true),
+        (_request: express.Request, response: express.Response) => {
+          response.sendFile('index.html', {
+            root: privatePath,
+          });
+        });
   }
 
   listen(): Promise<string> {

src/server/models/userModel.ts
@@ -14,9 +14,7 @@ export const TOKEN_COLLECTION_NAME = 'user-tokens';
 
 export const REQUIRED_SCOPES = ['repo'];
 
-export interface FeatureDetails {
-  enabledAt: number;
-}
+export interface FeatureDetails { enabledAt: number; }
 
 export interface UserRecord {
   githubToken: string;

src/test/pixel/pixel-test.ts
@@ -88,9 +88,9 @@ test.serial('[pixel-test] project-health1 dashboard UI', async (t) => {
 });
 
 
-test.serial('[pixel-test] project-health1 outgoing UI', async (t) => {
+test.serial('[pixel-test] project-health1 all outgoing UI', async (t) => {
   const page = await browser.newPage();
-  await page.goto(`${dashAddress}/outgoing`, {waitUntil: 'networkidle0'});
+  await page.goto(`${dashAddress}/all`, {waitUntil: 'networkidle0'});
 
   // Hide time stamps from screenshots.
   await page.$$eval('time', /* istanbul ignore next */ (elements) => {