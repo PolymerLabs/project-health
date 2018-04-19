1bdbe01c75a919e6367cd06a76c1c942703e9ffa
Sam Li <samli@google.com>
2018-04-19 12:53:40 -0700

    Add repos to the nav

--- src/client/public/scripts/components/nav-element.ts
+++ src/client/public/scripts/components/nav-element.ts
@@ -5,38 +5,62 @@ import * as api from '../../../../types/api.js';
 import {BaseElement, property} from './base-element.js';
 
 export class NavElement extends BaseElement {
-  @property() user: api.DashboardUser|null = null;
+  @property() data: api.UserResponse|null = null;
+
+  async connectedCallback() {
+    const response = await fetch('/api/user', {credentials: 'include'});
+    this.data = (await response.json()).data as api.UserResponse;
+  }
 
   _userTemplate(): TemplateResult {
-    if (!this.user) {
+    if (!this.data) {
       return html``;
     }
 
-    let avatarUrl = '/images/default-avatar.svg';
-    if (this.user.avatarUrl) {
-      avatarUrl = this.user.avatarUrl;
-    } else if (!this.user.isCurrentUser) {
-      avatarUrl = '/images/incognito.svg';
-    }
-
     return html`
-<a class="nav-item selected" href="/">
-  <img class="nav-item__avatar" src="${avatarUrl}"
-       alt="Avatar of ${this.user.login}" />
-  <div class="nav-item__name">${this.user.login}</div>
-</a>`;
+      <a class="nav-item selected" href="/">
+        <img class="nav-item__avatar" src="${this.data.avatarUrl}"
+            alt="Avatar of ${this.data.login}" />
+        <div class="nav-item__name">${this.data.login}</div>
+      </a>
+    `;
+  }
+
+  _header(): TemplateResult {
+    return html`
+      <div class="nav-item nav-title">
+        <img class="nav-item__avatar" src="/images/favicon.svg">
+        <div class="nav-item__name">Project Health</div>
+      </div>
+    `;
+  }
+
+  _repoTemplate(repo: api.Repository): TemplateResult {
+    return html`
+      <a class="nav-item" href="/repo/${repo.owner}/${repo.name}">
+        <img class="nav-item__avatar" src="${repo.avatarUrl}">
+        <div class="nav-item__name">${repo.name}</div>
+      </a>
+    `;
   }
 
   render() {
+    if (!this.data) {
+      return html`<nav>${this._header()}</nav>`;
+    }
+
     return html`
-<nav>
-  <div class="nav-item nav-title">
-    <img class="nav-item__avatar" src="/images/favicon.svg">
-    <div class="nav-item__name">Project Health</div>
-  </div>
-  ${this._userTemplate()}
-  <div class="nav-item__separator"></div>
-<nav>`;
+      <nav>
+        ${this._header()}
+        ${this._userTemplate()}
+
+        <div class="nav-item__separator"></div>
+
+        ${this.data.repos.map(this._repoTemplate)}
+
+        <div class="nav-item__separator"></div>
+      <nav>
+    `;
   }
 }
 
