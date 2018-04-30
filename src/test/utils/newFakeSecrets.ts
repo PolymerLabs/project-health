9595ee85d7bb26071b6e9a4cefe377a7162a7d33
Sam Li <samli@google.com>
2018-04-30 16:03:36 -0700

    Add production app (#498)

--- src/test/utils/newFakeSecrets.ts
+++ src/test/utils/newFakeSecrets.ts
@@ -7,9 +7,11 @@ export function newFakeSecrets(): ApplicationSecrets {
     PUBLIC_VAPID_KEY:
         'BPtJjYprRvU3TOb0tw3FrVbLww3bp7ssGjX99PFlqIOb3b8uOH4_Q21GYhwsDRwcfToaFVVeOxWOq5XaXD1MGdw',
     PRIVATE_VAPID_KEY: 'o1P9aXm-QPZezF_8b7aQabivhv3QqaB0yg5zoFs6-qc',
-    GITHUB_APP_TO_GQL_TOKEN: 'GitHubAppToGQLToken',
-    GITHUB_APP_ID: 0,
-    GITHUB_APP_JWT_PATH: '',
+    GITHUB_APP: {
+      TO_GQL_TOKEN: 'GitHubAppToGQLToken',
+      ID: 0,
+      JWT_PATH: '',
+    },
   };
   return Object.assign({}, fakeSecrets);
 }
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -1,11 +1,13 @@
+
 export type ApplicationSecrets = {
   GITHUB_CLIENT_ID: string; GITHUB_CLIENT_SECRET: string;
   PUBLIC_VAPID_KEY: string;
   PRIVATE_VAPID_KEY: string;
+  GITHUB_APP: GitHubAppSecrets;
+};
 
-  GITHUB_APP_TO_GQL_TOKEN: string;
-  GITHUB_APP_ID: number;
-  GITHUB_APP_JWT_PATH: string;
+type GitHubAppSecrets = {
+  TO_GQL_TOKEN: string; ID: number; JWT_PATH: string;
 };
 
 let secretsSingleton: ApplicationSecrets|null = null;
@@ -20,6 +22,15 @@ export function secrets(): ApplicationSecrets {
 export function initSecrets(secrets: ApplicationSecrets) {
   if (secretsSingleton) {
     throw new Error('Secrets already initialised.');
+  }
+  if (!secrets.GITHUB_APP && process.env.GAE_APPLICATION === 'github-health') {
+    secrets.GITHUB_APP = (secrets as ApplicationSecrets & {
+                           'GITHUB_APP_PROD': GitHubAppSecrets
+                         }).GITHUB_APP_PROD;
+  } else if (!secrets.GITHUB_APP) {
+    secrets.GITHUB_APP = (secrets as ApplicationSecrets & {
+                           'GITHUB_APP_STAGING': GitHubAppSecrets
+                         }).GITHUB_APP_STAGING;
   }
   secretsSingleton = secrets;
 }
